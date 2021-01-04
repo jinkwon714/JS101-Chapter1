60228df161fafce81ff969d1db58e449ff70fdef
Hyukjin Kwon <hjnim0909@gmail.com>
2021-01-05 00:15:05 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// 1. Tag를 가져온다.
+// const tag = document.querySelector(".tag");
+
+// function loadingTag() {
+//   tag.innerHTML = `
+//   window.screen: ${window.screen.width}, ${window.screen.height} <br/>
+//   window.outer: ${window.outerWidth}, ${window.outerHeight} <br/>
+//   window.inner: ${window.innerWidth}, ${window.innerHeight} <br/>
+//   documentElement.clientWidth: ${document.documentElement.clientWidth}
+//   `;
+// }
+
+// window.addEventListener("resize", () => {
+//   loadingTag();
+// });
+// loadingTag();
