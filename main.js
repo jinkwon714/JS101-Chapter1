311c18cd3efb63655ecf53d2dbfe3f65a6765eba
Hyukjin Kwon <hjnim0909@gmail.com>
2021-01-06 02:20:02 +0900

    2. Window-coordinates
    - Updated practicing on my own
    - Need to make codes on the console display of Chrome

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-// 1. Tag를 가져온다.
-// const tag = document.querySelector(".tag");
+//1. addEventListener 만들고 그에 해당하는 function을 2번으로 만들기
+document.addEventListener("click", printMouse);
 
-// function loadingTag() {
-//   tag.innerHTML = `
-//   window.screen: ${window.screen.width}, ${window.screen.height} <br/>
-//   window.outer: ${window.outerWidth}, ${window.outerHeight} <br/>
-//   window.inner: ${window.innerWidth}, ${window.innerHeight} <br/>
-//   documentElement.clientWidth: ${document.documentElement.clientWidth}
-//   `;
-// }
+//2. x,y를 보여줄 수 있는 function을 만들어서 addEventListner에 넣기
+function printMouse(event) {
+  document.body.textContent = `clientX: ${event.clientX}
+  clientY: ${event.clientY}`;
+  //"clientX: " + event.clientX + "-clientY:" + event.clientY;
+}
 
-// window.addEventListener("resize", () => {
-//   loadingTag();
-// });
-// loadingTag();
+// getBoundingClientRect() 써보기
+
+let elem = document.querySelector(".box").getBoundingClientRect();
+console.log(elem);
